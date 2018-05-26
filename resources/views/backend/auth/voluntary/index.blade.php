79fade8882aef738b35e760e5852447caed60195
hederithamar <heder.ithamar.hr@gmail.com>
2018-05-26 06:15:30 -0500

    cambio

--- resources/views/backend/auth/voluntary/index.blade.php
+++ resources/views/backend/auth/voluntary/index.blade.php
@@ -22,7 +22,84 @@
         </div><!--row-->
 
         <div class="row mt-12">
-            
+            @foreach ($voluntaries as $voluntary)
+<div class="col col-sm-3 order-1 order-sm-3  mb-3">
+    <div class="card-body">
+        <ul class="nav nav-tabs" id="myTab{{$voluntary->id}}" role="tablist">
+        <li class="nav-item">
+            <a class="nav-link active" id="home-tab" data-toggle="tab" href="#home{{$voluntary->id}}" role="tab" aria-controls="home" aria-selected="true"><i class="fa fa-heart"></i></a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile{{$voluntary->id}}" role="tab" aria-controls="profile" aria-selected="false"><i class="fa fa-info-circle"></i></a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" id="contact-tab" data-toggle="tab" href="#contact{{$voluntary->id}}" role="tab" aria-controls="contact" aria-selected="false"><i class="fa fa-map-marker"></i></a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" id="action-tab" data-toggle="tab" href="#action{{$voluntary->id}}" role="tab" aria-controls="action" aria-selected="false"><i class="fa fa-newspaper-o"></i></a>
+        </li>
+        </ul>
+        <div class="tab-content" id="myTab{{$voluntary->id}}Content">
+            <div class="tab-pane fade show active" id="home{{$voluntary->id}}" role="tabpanel" aria-labelledby="home-tab">
+                <h5>
+                    <small class="text-muted">
+                      {{ $voluntary->celphone }}<br/>
+                    </small>
+                </h5>
+                
+                <img class="card-img-top" src="{{ $voluntary->picture }}" alt="Profile Picture" height="150" width="150">
+            </div>
+
+            <div class="tab-pane fade" id="profile{{$voluntary->id}}" role="tabpanel" aria-labelledby="profile-tab">
+                
+                <div class="callout callout-info">
+                    <strong class="h4">{{ $voluntary->sexo }}</strong>
+                    <br>
+                    <small class="text-muted"><i class="fa fa-info"></i> {{ $voluntary->facebook }}</small>
+                    <br>
+                    <small class="text-muted"><i class="fa fa-user"></i> {{ $voluntary->user->name }}</small>
+                    <br>
+                    <div class="chart-wrapper">
+                        <canvas id="sparkline-chart-1" width="100" height="30"></canvas>
+                    </div>
+                   
+                </div>
+            </div>
+            <div class="tab-pane fade" id="contact{{$voluntary->id}}" role="tabpanel" aria-labelledby="contact-tab">
+               
+                <div class="callout callout-warning">
+                    <small class="text-muted"><i class="fa fa-map"></i>  {{ $voluntary->escolaridad }}</small>
+                    <br>
+                    <small class="text-muted"><i class="fa fa-calendar"></i>  {{ $voluntary->carrera }}</small>
+                    <br>
+                    <br>
+                    <strong class="h4">{{ $voluntary->status }}</strong>
+                    
+                    <div class="chart-wrapper">
+                        <canvas id="sparkline-chart-1" width="100" height="30"></canvas>
+                    </div>
+                </div>
+            </div>
+
+            <div class="tab-pane fade" id="action{{$voluntary->id}}" role="tabpanel" aria-labelledby="action-tab">
+                @if ($voluntary->status != "En espera")
+                    <div class="callout callout-success">
+                        <strong class="h4"><i class="fa fa-check"></i> Tus servicio son requeridos, te contactaremos lo mas breve posible. {{ $voluntary->evidence_text }}</strong>
+                        
+                       
+                    </div>
+                @else
+                    <div class="callout callout-warning">
+                    <strong class="h4"><i class="fa fa-info"></i> Estamos procesando tu solicitud espera respuesta :)</strong>
+                    </div>
+                @endif
+               
+            </div>
+
+        </div>
+    </div>
+</div><!--col-md-4-->
+@endforeach
                                
         </div><!--row-->
         <div class="row">
